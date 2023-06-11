f9642138826597a8ae01410b9750f68546f3cbbd
Jean-Luc LYS <jeanluc.lys@gmail.com>
2023-06-11 11:46:54 +0200

    add design.md and update main

---
+++ design.md
@@ -0,0 +1,10 @@
+## Pre-design schema
+
+![img.png](schema.png)
+
+## Big ideas of your design, key building blocs
+
+- Hexagonal Architecture (application core, domain, infrastructure)
+- CQRS Pattern (create, retrieve, update, delete)
+- DDD
+
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,9 @@
-import main.application.command.create.CreateTaskHandler;
-import main.application.command.delete.DeleteTaskHandler;
-import main.application.command.update.UpdateTaskHandler;
-import main.application.query.retrieve.RetrieveTasksSortedByCreationDate;
-import main.application.query.retrieve.RetrieveTasksSortedByCreationDateHandler;
-import main.domain.model.State;
-import main.domain.model.Task;
 import main.infrastructure.CommandLineApp;
 import main.infrastructure.CsvFileHandler;
 import main.kernel.FileHandler;
-import main.kernel.exception.EmptyFileException;
 import org.apache.commons.csv.CSVFormat;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.Scanner;
-import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -31,17 +17,6 @@ public class Main {
         scanner.close();
 
         commandLineApp.run(commandLine);
-
-        UpdateTaskHandler updateTaskHandler = new UpdateTaskHandler(fileHandler);
-        DeleteTaskHandler deleteTaskHandler = new DeleteTaskHandler(fileHandler);
-        RetrieveTasksSortedByCreationDateHandler retrieveTasksSortedByCreationDateHandler = new RetrieveTasksSortedByCreationDateHandler(fileHandler, fileHandler.getTasks());
-        CreateTaskHandler createTaskHandler = new CreateTaskHandler(fileHandler);
-
-        UUID id = UUID.randomUUID();
-        Task task = new Task(id, new Date(), new Date(), new Date(), "description", State.TODO, new ArrayList<>());
-
-        RetrieveTasksSortedByCreationDate retrieveTasksSortedByCreationDate = new RetrieveTasksSortedByCreationDate();
-        //System.out.println(retrieveTasksSortedByCreationDateHandler.handle(retrieveTasksSortedByCreationDate));
 
     }
 }
